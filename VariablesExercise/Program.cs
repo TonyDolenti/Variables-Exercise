403b52264c58cb2e966382db00f036ca43b32d95
TonyDolenti <antoniobdolenti@gmail.com>
2023-01-24 22:31:23 -0500

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string favChestExercise = "bench press";
+            int chestExerciseWeight = 185;
+            char lastInitial = 'D';
+            bool isOver18 = true;
+            double age = 20;
+            decimal bodyFatPercentage = 19.05m;
+
+            Console.WriteLine($"Favorite chest exercise: {favChestExercise}");
+            Console.WriteLine($"With how much weight: {chestExerciseWeight}");
+            Console.WriteLine($"Initial of last name: {lastInitial}");
+            Console.WriteLine($"Is over 18: {isOver18}");
+            Console.WriteLine($"Specific age: {age}");
+            Console.WriteLine($"Body fat percentage: {bodyFatPercentage}");
         }
     }
 }
